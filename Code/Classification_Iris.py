f3edcc241df595dfcf65b63a294f295d6be0d1f2
M-Yazdan <77092746+M-Yazdan@users.noreply.github.com>
2021-02-09 23:25:29 +0500

    Initial  commit

---
+++ Code/Classification_Iris.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  4 18:13:51 2021
+
+@author: myazd
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 16 07:57:22 2020
+
+@author: myazd
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import matplotlib.pyplot as plt                   # For graphics
+import seaborn as sns
+from sklearn.cluster import KMeans
+
+ds = pd.read_csv('Iris.csv')
+x=ds.iloc[:,:-1].values
+y=ds.iloc[:,5].values
+
+
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test=train_test_split(x,y,test_size=0.3, random_state=0)
+
+
+from sklearn.preprocessing import StandardScaler
+sc=StandardScaler()
+x_train=sc.fit_transform(x_train)
+x_test=sc.transform(x_test)
+
+from sklearn.cluster import KMeans
+wcss = []
+
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters = i, init = 'k-means++', max_iter = 300, n_init = 10, random_state = 0)
+    kmeans.fit(x)
+    wcss.append(kmeans.inertia_)
+    
+#Plotting the results onto a line graph, allowing us to observe 'The elbow'
+plt.plot(range(1, 11), wcss)
+plt.title('The elbow method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS') #within cluster sum of squares
+plt.show()
+
+y_pred= classifer.predict(x_test)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test,y_pred)
+
+
+from sklearn.metrics import accuracy_score
+acc=accuracy_score(y_test,y_pred)
+
+
+
+# Fitting K-Means to the dataset
+kmeans = KMeans(n_clusters = 3, init = 'k-means++', random_state = 0)
+y_kmeans = kmeans.fit_predict(x)
+
+#Visualising the clusters
+plt.scatter(x[y_kmeans == 0, 0], x[y_kmeans == 0, 1], s = 100, c = 'red', label = 'Iris-setosa')
+plt.scatter(x[y_kmeans == 1, 0], x[y_kmeans == 1, 1], s = 100, c = 'blue', label = 'Iris-versicolour')
+plt.scatter(x[y_kmeans == 2, 0], x[y_kmeans == 2, 1], s = 100, c = 'green', label = 'Iris-virginica')
+
+#Plotting the centroids of the clusters
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:,1], s = 100, c = 'yellow', label = 'Centroids')
+
+plt.legend()
